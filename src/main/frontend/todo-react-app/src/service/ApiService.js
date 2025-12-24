934c3369020ec2c8d293a89d5589384386758ccb
YJ <chlrhhan@gmail.com>
2025-12-24 15:21:56 +0900

    feat : localStorage에 액세스 token 저장 및 가져오기 개발

--- src/main/frontend/todo-react-app/src/service/ApiService.js
+++ src/main/frontend/todo-react-app/src/service/ApiService.js
@@ -1,10 +1,21 @@
 import {API_BASE_URL} from "../api-config";
 
+const ACCESS_TOKEN = "ACCESS_TOKEN";
+
 export function call(api, method, request) {
+
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+
+    // 로컬 스토리지에서 ACCESS_TOKEN 가져오기
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if (accessToken && accessToken !== null) {
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
     const options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -35,6 +46,8 @@ export function signin(userDto) {
     return call("/auth/signin", "POST", userDto)
         .then((response) => {
             if (response.token) {
+                // 로컬 스토리지에 토큰 저장
+                localStorage.setItem("ACCESS_TOKEN", response.token);
                 // token이 존재하는 경우 메인 화면으로 리다이렉트
                 window.location.href = "/";
             }
