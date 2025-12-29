2f0748f2f45e3c2fbf5767c45889d61958ecb2cd
YJ <chlrhhan@gmail.com>
2025-12-29 22:54:28 +0900

    feat : logout 개발

--- src/main/frontend/todo-react-app/src/service/ApiService.js
+++ src/main/frontend/todo-react-app/src/service/ApiService.js
@@ -53,3 +53,8 @@ export function signin(userDto) {
             }
         });
 }
+
+export function signout() {
+    localStorage.setItem(ACCESS_TOKEN, null);
+    window.location.href = "/login";
+}
