1ebc2936831409a215d9e1089f05fc9d7934cbac
YJ <chlrhhan@gmail.com>
2026-01-09 13:51:50 +0900

    feat : aws 배포시 backend 주소 세팅

--- src/main/frontend/todo-react-app/src/api-config.js
+++ src/main/frontend/todo-react-app/src/api-config.js
@@ -4,8 +4,9 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
     backendHost = "http://localhost:1234";
+} else {
     // aws 배포 시
-    // backendHost = "http://prod-todo-backend.us-west-2.elasticbeanstalk.com";
+    backendHost = "http://prod-todo-backend.us-west-2.elasticbeanstalk.com"; // aws 주소로 변경
 }
 
 export const API_BASE_URL = `${backendHost}`
