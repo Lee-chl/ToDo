8b258113f84cf3a11ce5039a3f2bd76a23955705
YJ <chlrhhan@gmail.com>
2026-01-12 15:48:53 +0900

    feat : 서브 도메인 생성 후 주소 변경

--- src/main/frontend/todo-react-app/src/api-config.js
+++ src/main/frontend/todo-react-app/src/api-config.js
@@ -6,7 +6,7 @@ if (hostname === "localhost") {
     backendHost = "http://localhost:1234";
 } else {
     // aws 배포 시
-    backendHost = "http://prod-todo-backend.us-west-2.elasticbeanstalk.com"; // aws 주소로 변경
+    backendHost = "http://api.<도메인>"; // 서브 도메인 추가 후 주소 변경
 }
 
 export const API_BASE_URL = `${backendHost}`
